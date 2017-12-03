ac24a4968d9fedfc4f0716260726283ee09f3cb5
carriesampson <i.carrie.sampson@gmail.com>
2017-12-02 22:33:02 -0500

    EJS for index and show pages complete; tested and launched successfully

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,8 +11,7 @@ const Comments = require("../models/comments.js");
 //INDEX ROUTE
 router.get('/', async (req, res) => {
   const allPosts = await Posts.find();
-  res.send(allPosts)
-  // res.render("../views/home/index.ejs", { allPosts });
+  res.render("../views/home/index.ejs", { allPosts });
 });
 
 // //CREATE ROUTE (APP CREATED PHOTO)
@@ -24,19 +23,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const onePost = await Posts.findById(req.params.id);
   const comments = await Comments.find({post: onePost._id});
-  res.send({onePost, comments});
-  // res.render("../views/home/show.ejs", {onePost, comments});
+  res.render("../views/home/show.ejs", {onePost, comments});
 });
 
-//DB SEEDED CREATE ROUTE
-router.post('/', async (req, res) => {
-  try {
-    const createdPost = await Posts.create(req.body);
-    res.redirect('back');
-  } catch (err) {
-    res.send(err.message);
-  }
-});
+// //DB SEEDED CREATE ROUTE
+// router.post('/', async (req, res) => {
+//   try {
+//     const createdPost = await Posts.create(req.body);
+//     res.redirect('back');
+//   } catch (err) {
+//     res.send(err.message);
+//   }
+// });
 
 //EXPORTS
 module.exports = router;
