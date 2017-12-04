bfbbad9d68fd5c78e67777301cf52653419aaf69
carriesampson <i.carrie.sampson@gmail.com>
2017-12-04 17:17:43 -0500

    Created registration model/schema file

--- models/reg.js
+++ models/reg.js
@@ -1,14 +1,13 @@
-// //DEPENDENCIES
-// const mongoose = require("mongoose");
-// 
-// //REGISTRATION SCHEMA
-// const regSchema = mongoose.Schema({
-//   emailAddress: {type:String, require: true},
-//   retypeEmailAddress: {type:String, require: true},
-//   username: {type: String, require: true},
-//   password: {type: String, require: true},
-//   retypePassword: {type: String, require: true}
-// });
-//
-// //EXPORTS
-// module.exports = mongoose.model("Register", regSchema);
+//DEPENDENCIES
+const mongoose = require("mongoose");
+
+//REGISTRATION SCHEMA
+const regSchema = mongoose.Schema({
+  emailAddress: {type:String, require: true},
+  username: {type: String, require: true},
+  password: {type: String, require: true},
+  retypePassword: {type: String, require: true}
+});
+
+//EXPORTS
+module.exports = mongoose.model("Reg", regSchema);
